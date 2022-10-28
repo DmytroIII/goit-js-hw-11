a163ba7b7506c67bb6d76cf48219c770fce74a28
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-10-28 21:43:22 +0300

    dz

---
+++ src/index.js
@@ -0,0 +1,102 @@
+
+
+import SmoothScroll from "smoothscroll-for-websites";
+import SimpleLightbox from "simplelightbox";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import PixabayApiService from './js/pixabay-service.js'
+import markupGallery from './templates/card.hbs';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const refs = {
+    gallery: document.querySelector('.js-gallery'),
+    form: document.querySelector('.js-search-form'),
+    guard: document.querySelector('.guard'),
+};
+refs.form.addEventListener('submit', onSearchSubmit);
+
+const pixabayApiService = new PixabayApiService();
+const gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+// --------- SmoothScroll ---------- //
+
+SmoothScroll({
+    stepSize: 175,
+    animationTime: 800,
+    accelerationDelta: 200,
+    accelerationMax: 6,
+    keyboardSupport: true,
+    arrowScroll: 100,
+});
+
+
+// ------ Infinity scroll setup ------
+const options = {
+    root: null,
+    rootMargin: '200px',
+    threshold: 1,
+}
+const observer = new IntersectionObserver(onLoad, options);
+// ----------------------------
+
+
+function onSearchSubmit(e) {
+    e.preventDefault();
+    clearGallery();
+
+    pixabayApiService.query = refs.form.elements.searchQuery.value.trim()
+    if (pixabayApiService.query === '') {
+        Notify.failure('It cannot be empty field')
+        return;
+    }
+    pixabayApiService.resetPage();
+    pixabayApiService.fetchPictures().then(checkAndAppendMarkup);
+    e.currentTarget.reset()
+}
+
+
+function checkAndAppendMarkup(resultPromise) {
+    if (resultPromise.hits.length != 0) {
+        Notify.success(`Hooray! We found ${resultPromise.totalHits} images.`);
+        appendCardsMarkup(resultPromise);
+        observer.observe(refs.guard);
+    } else {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        return
+    }
+}
+// ----- infinity scroll ----//
+function onLoad(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            onLoadMore();
+        }
+    })
+}
+
+
+function onLoadMore() {
+    let currentPage = pixabayApiService.currentPage;
+    let totalPages = pixabayApiService.allPages;
+    if (currentPage > totalPages) {
+        Notify.failure("We're sorry, but you've reached the end of search results.");
+        observer.unobserve(refs.guard)
+    } else {
+        return pixabayApiService.fetchPictures().then(appendCardsMarkup);
+    }
+}
+//-----------------------------------//
+function appendCardsMarkup(card) {
+    if (pixabayApiService.query === '') {
+        return;
+    }
+    refs.gallery.insertAdjacentHTML('beforeend', markupGallery(card))
+    gallerySimpleLightbox.refresh();
+}
+
+function clearGallery() {
+    refs.gallery.innerHTML = '';
+}
---
+++ src/js/pixabay-service.js
@@ -0,0 +1,67 @@
+
+
+
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '30885828-3a5027e794685de5837ffa869';
+
+export default class axiosApi {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.perPage = 40;
+        this.totalPages = 0;
+    }
+    async fetchPictures() {
+        try {
+            const response = await axios.get(BASE_URL, {
+                params: {
+                    key: KEY,
+                    q: this.searchQuery,
+                    image_type: 'photo',
+                    orientation: 'horizontal',
+                    safesearch: 'true',
+                    page: this.page,
+                    per_page: this.perPage,
+                }
+            });
+            this.incrementPage();
+            const data = await response.data;
+            let pages = Math.ceil(data.totalHits / data.hits.length);
+            this.totalPages = pages;
+            return data
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+    incrementPage() {
+        this.page += 1;
+    }
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+
+    get currentPage() {
+        return this.page
+    }
+
+    set currentPage(value) {
+        this.page = value;
+    }
+
+    get allPages() {
+        return this.totalPages;
+    }
+    set allPages(value) {
+        this.totalPages = value;
+    }
+}
